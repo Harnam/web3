0e58e345fd28e61f9c1066741ceeee468290c16e
Harnam Singh <harnamsinghsandhu04@gmail.com>
2022-07-14 19:39:25 +0530

    added components and basic

--- frontAngular/src/app/app.module.ts
+++ frontAngular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
